d608f7157181879698f02a0cde2a7fcf50576293
Eik Hvattum Røgeberg <eik.rogeberg@gmail.com>
2023-02-27 19:36:11 +0100

    Better dependencies

--- server/app.js
+++ server/app.js
@@ -43,5 +43,5 @@ app.post('/', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Subtitle server listening on port ${port}`);
 });
